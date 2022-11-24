6da1ed70c3be22b1dc930497e95ba1db83e6b732
Sarthak Singh <61286870+Sarthak-125@users.noreply.github.com>
2022-11-24 19:39:15 +0530

    Update infer_functions.py

--- infer_functions.py
+++ infer_functions.py
@@ -210,11 +210,11 @@ def infer(nlu1,
     pr_sql_q1 = generate_sql_q(pr_sql_i, tb)
     pr_sql_q = [pr_sql_q1]
 
-    print(f'START ============================================================= ')
-    print(f'{hds}')
-    print(f'nlu: {nlu}')
-    print(f'pr_sql_i : {pr_sql_i}')
-    print(f'pr_sql_q : {pr_sql_q}')
-    print(f'---------------------------------------------------------------------')
+    #print(f'START ============================================================= ')
+    #print(f'{hds}')
+    #print(f'nlu: {nlu}')
+    #print(f'pr_sql_i : {pr_sql_i}')
+    #print(f'pr_sql_q : {pr_sql_q}')
+    #print(f'---------------------------------------------------------------------')
 
     return pr_sql_i, pr_sql_q1
